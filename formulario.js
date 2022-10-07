43876c279debde97f2b4340e71a8f59e5484f169
Bruno-Giachetti <bgiachetti@frba.utn.edu.ar>
2022-10-07 18:00:45 -0300

    Añado verificacion (?) y colores

--- formulario.js
+++ formulario.js
@@ -1,3 +1,33 @@
-function alertar(){
-   alert("Formulario enviado");
-}
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+function validar() {
+   'use strict'
+
+ 
+   // Fetch all the forms we want to apply custom Bootstrap validation styles to
+   var forms = document.querySelectorAll('.needs-validation')
+
+   // Loop over them and prevent submission
+   let i = 0;
+   Array.prototype.slice.call(forms)
+     .forEach(function (form) {
+       form.addEventListener('submit', function (event) {
+         if (!form.checkValidity()) {
+           event.preventDefault()
+           event.stopPropagation()
+         }
+         else {
+         if(i===0)
+         {
+            console.log(i)
+            alert("Formulario Enviado")
+            i++
+            
+         }
+      }
+ 
+         form.classList.add('was-validated')
+       }, false)
+     
+     })
+     
+ }
