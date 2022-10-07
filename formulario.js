63b02f844a80fab8c8cf1986f20fb0eae5480f6e
Bruno-Giachetti <bgiachetti@frba.utn.edu.ar>
2022-10-07 18:56:17 -0300

    Añado twitter y linea borde a dd

--- formulario.js
+++ formulario.js
@@ -2,25 +2,24 @@
 function validar() {
    'use strict'
 
- 
    // Fetch all the forms we want to apply custom Bootstrap validation styles to
    var forms = document.querySelectorAll('.needs-validation')
 
    // Loop over them and prevent submission
-   let i = 0;
    Array.prototype.slice.call(forms)
      .forEach(function (form) {
        form.addEventListener('submit', function (event) {
+        let i = true
          if (!form.checkValidity()) {
            event.preventDefault()
            event.stopPropagation()
          }
          else {
-         if(i===0)
+         if(i)
          {
             console.log(i)
             alert("Formulario Enviado")
-            i++
+            i=false;
             
          }
       }
